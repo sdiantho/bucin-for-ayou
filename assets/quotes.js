e82b6c63a83ff2e13f474821dfbbac61a9d72d98
sdiantho <62882118+sdiantho@users.noreply.github.com>
2021-03-22 15:23:46 +0700

    quotes.js

--- assets/quotes.js
+++
@@ -1,5 +0,0 @@
-// prettier-ignore
-export default [
-    { author: "sd.antho", quotes: "don't worry i'm still waitin time perfect" },
-    { author: "sd.antho", quotes: "The Earth's in rotation, i waitin' for you" },
-    { author: "sd.antho", quotes: "Hey! I wuv chuuuu! UwU" },
